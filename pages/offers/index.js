4e7b6cd1e858ac7886bd8198768617078b9f7154
oscar-halcon <oscar@secfi.com>
2022-09-29 16:37:51 +0200

    again

--- pages/offers/index.js
+++ pages/offers/index.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 const Offers = () => {
     return (
       <div>
         <h1>All Offers</h1>
+        <Link href={'/offers/' + '2'} key="2">
+          <a>Offer 1</a>
+        </Link>
       </div>
     );
   }
