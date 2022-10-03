61c7876a7313995431c82cfd38baba447e6602a6
oscar-halcon <oscar@secfi.com>
2022-10-03 14:00:23 +0200

    Fixes and showing information of each offer

--- pages/offers/[id].js
+++ pages/offers/[id].js
@@ -1,23 +1,61 @@
 
-export const getStaticPaths = async () => {
-    const res = await fetch('/JSONS/OfferSkeleton');
-    const data = await res.json();
+import Offers from "../../JSONS/OfferSkeleton.json";
 
+export const getStaticPaths = async () => {
+    const data =  Offers;
     const paths = data.map(offer =>{
-        params: {id: offer.id.toString()}
+        return{
+            params: {id: offer.id.toString()}
+        }
     })
     return {
         paths,
         fallback: false
     }
 }
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const data = Offers.at(id-1);
+    return{
+        props:{ offer: data}
+    }
+}
 
-const Details = () => {
+const Details = ({offer}) => {
     return ( 
         <div>
-            <h1>Details</h1>
+            <table>
+                <tr>
+                    <th>ID</th><th>{offer.id}</th>
+                </tr>
+                <tr>
+                    <th>Annual Salary</th><th>{offer.Annual_salary}</th>
+                </tr>
+            </table>
+            <h4>ID: {offer.id}</h4>
+            <h4>Annual Salary: {offer.Annual_salary}</h4>
+            <h4>Bonus: {offer.Bonus}</h4>
+            <h4>Total Annual: {offer.Annual_salary + offer.Bonus}</h4>
+            <h4>Strike Price: {offer.Strike_price}</h4>
+            <h4>Number Options: {offer.Number_options}</h4>
+            <h4>Current FMV: {offer.Current_FMV_company}</h4>
+            <h4>Cost of exercising: {offer.Strike_price * offer.Number_options}</h4>
+            <h4>Value equity in offer: {offer.Number_options * offer.Current_FMV_company}</h4>
+            <h4>Company Increase x5: {((offer.Number_options * offer.Current_FMV_company)*5)}</h4>
+            <h4>Company Falls x5: {(offer.Number_options * offer.Current_FMV_company)/5}</h4>
+            <h4>401k matching: {offer.matching_401k}</h4>
+            <h4>Office food: {offer.Office_food}</h4>
+            <h4>Covers tranport costs: {offer.Cover_Transport_costs}</h4>
+            <h4>Health insurance: {offer.Health_insurance}</h4>
+            <h4>Vacation days: {offer.Vacation_days}</h4>
+            <h4>Parental leave days: {offer.Parental_leave}</h4>
+            <h4>Training budget: {offer.Training_budget}</h4>
+            <h4>Home office budget: {offer.Home_office_budget}</h4>
+            <h4>Remote working: {offer.Remote_working}</h4>
+            <h4>Growth plan: {offer.Growth_plan}</h4>
         </div>
      );
+     //<input type="checkbox"></input> for when I need the user to put the data
 }
  
 export default Details;
--- pages/offers/index.js
+++ pages/offers/index.js
@@ -1,12 +1,24 @@
 import Link from "next/link";
 
-const Offers = () => {
+import Offer from '../../JSONS/OfferSkeleton.json';
+
+export const getStaticProps = async () => {
+  const data = Offer;
+  return{
+    props: { offer: data}
+  }
+}
+
+const Offers = ({offer}) => {
     return (
       <div>
         <h1>All Offers</h1>
-        <Link href={'/offers/' + '2'} key="2">
-          <a>Offer 1</a>
-        </Link>
+        {offer.map(a=>(
+         <Link href={'/offers/' + a.id} key={a.id}>
+         <a>Offer {a.id}</a>
+       </Link> 
+        ))}
+        
       </div>
     );
   }
