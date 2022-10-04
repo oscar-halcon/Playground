1ae61b22ea57004c052ec03341809a8f9401c0e2
oscar-halcon <oscar@secfi.com>
2022-10-04 10:13:27 +0200

    Vercel test 3

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ const NoAdres = () => {
     return ( 
         <div className="404">
             <h4>404</h4>
-            <h1>Strange this page doesn't exist</h1>
+            <h1>Strange this page does not exist</h1>
             <h2>You can go back to <Link href='/'><a>Home</a></Link></h2>
         </div>
      );
