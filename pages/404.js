a2a1994fdb22fa95b44106fa88a1c56169ed87aa
oscar-halcon <oscar@secfi.com>
2022-10-04 10:18:26 +0200

    vercel 4

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,9 @@
-import Link from "next/link";
 
 const NoAdres = () => {
     return ( 
         <div className="404">
             <h4>404</h4>
             <h1>Strange this page does not exist</h1>
-            <h2>You can go back to <Link href='/'><a>Home</a></Link></h2>
         </div>
      );
 }
