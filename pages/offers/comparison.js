bcd2e859ff926edccf417ad22d221805d2e14c5d
oscar-halcon <oscar@secfi.com>
2022-10-04 14:20:16 +0200

    Show both offers at the same time comparison

--- pages/offers/comparison.js
+++ pages/offers/comparison.js
@@ -11,16 +11,82 @@ const comparison = ({offer1, offer2}) => {
     return ( 
         <div>
             <table className='comparetab'>
+                {//offer.map(a=>(<tr> <th>a.id</th></tr> <tr> <th>a.id</th></tr>))
+                }
                 <tr>
                     <th>ID</th><th>{offer1.id}</th>
                     <th>{offer2.id}</th>
                 </tr>
                 <tr>
-                    <th>Annual Salary</th><th>{offer1.Annual_salary}</th>
-                    <th>{Number(offer2.Annual_salary)}</th>
+                    <th>Total Annual</th>
+                    <th>{offer1.Annual_salary+offer1.Bonus}</th>
+                    <th>{offer2.Annual_salary+offer2.Bonus}</th>
+                </tr>
+                <tr>
+                    <th>Number Options</th><th>{offer1.Number_options}</th>
+                    <th>{offer2.Number_options}</th>
+                </tr>
+                <tr>
+                    <th>Cost Exercising</th>
+                    <th>{offer1.Number_options * offer1.Strike_price}</th>
+                    <th>{offer2.Number_options * offer2.Strike_price}</th>
+                </tr>
+                <tr>
+                    <th>Value of equity</th>
+                    <th>{offer1.Number_options * offer1.Current_FMV_company}</th>
+                    <th>{offer2.Number_options*offer2.Current_FMV_company}</th>
+                </tr>
+                <tr>
+                    <th>Increase value of the company</th>
+                </tr>
+                <tr>
+                    <th>Increment of company X3</th>
+                    <th>{(offer1.Number_options * offer1.Current_FMV_company)*3}</th>
+                    <th>{(offer2.Number_options*offer2.Current_FMV_company)*3}</th>
+                </tr>
+                <tr>
+                    <th>Increment of company X5</th>
+                    <th>{(offer1.Number_options * offer1.Current_FMV_company)*5}</th>
+                    <th>{(offer2.Number_options*offer2.Current_FMV_company)*5}</th>
+                </tr>
+                <tr>
+                    <th>Increment of company X7</th>
+                    <th>{(offer1.Number_options * offer1.Current_FMV_company)*7}</th>
+                    <th>{(offer2.Number_options*offer2.Current_FMV_company)*7}</th>
+                </tr>
+                <tr>
+                    <th>Fall of the company</th>
+                </tr>
+                <tr>
+                    <th>Decrement of company X7</th>
+                    <th>{Math.round((offer1.Number_options * offer1.Current_FMV_company)/7)}</th>
+                    <th>{Math.round((offer2.Number_options*offer2.Current_FMV_company)/7)}</th>
+                </tr>
+                <tr>
+                    <th>Decrement of company X5</th>
+                    <th>{(offer1.Number_options * offer1.Current_FMV_company)/5}</th>
+                    <th>{(offer2.Number_options*offer2.Current_FMV_company)/5}</th>
+                </tr>
+                <tr>
+                    <th>Decrement of company X10</th>
+                    <th>{(offer1.Number_options * offer1.Current_FMV_company)/10}</th>
+                    <th>{(offer2.Number_options*offer2.Current_FMV_company)/10}</th>
+                </tr>
+                <tr>
+                    <th>Extra Benefits</th>
+                </tr>
+                <tr>
+                    <th>Vacation days</th>
+                    <th>{offer1.Vacation_days}</th>
+                    <th>{offer2.Vacation_days}</th>
+                </tr>
+                <tr>
+                    <th>Parental Leave days</th>
+                    <th>{offer1.Parental_leave}</th>
+                    <th>{offer2.Parental_leave}</th>
                 </tr>
             </table>
-        </div>//<th>{Number(offer1.Annual_salary)-Number(offer2.Annual_salary)}</th>
+        </div>
      );
 }
  
