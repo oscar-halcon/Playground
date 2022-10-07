ba7735d9b54eb61a51485601166ee4ace6a061cf
oscar-halcon <oscar@secfi.com>
2022-10-07 12:23:42 +0200

    Comunication with Backend API GET/POST

--- pages/offers/index.js
+++ pages/offers/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import { useState } from "react";
 import Offer from "../../JSONS/OfferSkeleton.json";
 
 export const getStaticProps = async () => {
@@ -10,17 +10,60 @@ export const getStaticProps = async () => {
 };
 
 const Offers = ({ offer }) => {
+  const [offers, setOffers] = useState([]);
+  const [offers2, setOffers2] = useState("");
+  const [answer, setAnswers] = useState("");
+  const FetchApi2 = async () => {
+    const response = await fetch("/api/goodbye");
+    const data = await response.json();
+    setOffers2(data);
+  };
+  const FetchApi = async () => {
+    const response = await fetch("/api/hello");
+    const data = await response.json();
+    setOffers(data);
+  };
+  const SubmitApi2 = async () => {
+    const response = await fetch("/api/goodbye", {
+      method: "POST",
+      body: offers2,
+      headers: {'Content-Type': 'application/text'}
+    });
+    const data = await response.json();
+    setAnswers(data);
+  };
   return (
     <div>
       <h1>All Offers</h1>
       {offer.map((a) => (
         <Link href={"/offers/" + a.id} key={a.id}>
-          <a><h3>Offer {a.id}</h3></a>
+          <a>
+            <h3>Offer {a.id}</h3>
+          </a>
         </Link>
       ))}
       <Link href={"/offers/comparison"}>
         <a>Compare</a>
       </Link>
+      <button onClick={FetchApi}>Get API</button>
+      {offers.map((a) => {
+        return (
+          <Link href={"/offers/" + a.id} key={a.id}>
+            <a>
+              <h3>Offer {a.id}</h3>
+            </a>
+          </Link>
+        );
+      })}
+      <button onClick={FetchApi2}>Get API 2</button>
+      <h3>{offers2}</h3>
+      <input
+        type="text"
+        value={offers2}
+        onChange={(e) => setOffers2(e.target.value)}
+      />
+      <button onClick={SubmitApi2}>Change API 2</button>
+      <h3>{answer}</h3>
     </div>
   );
 };
