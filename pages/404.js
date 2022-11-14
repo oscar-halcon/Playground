6552158798dc2aee989216197d92cd4c616887bb
oscar-halcon <oscar@secfi.com>
2022-11-14 15:50:14 +0100

    Tries to see if tailwind works

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,8 @@
 const NoAdres = () => {
   return (
     <div className="404">
-      <h4>404</h4>
-      <h1>Strange this page does not exist</h1>
+      <h4 class="text-3xl font-bold underline">404</h4>
+      <h1 class="selection:bg-fuchsia-300 selection:text-fuchsia-900">Strange this page does not exist</h1>
     </div>
   );
 };
