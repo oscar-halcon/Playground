1cc016838e308e009652ce7dcbdb3e5f6980df3b
oscar-halcon <oscar@secfi.com>
2022-11-17 14:18:46 +0100

    wip/ChangetoTailwind

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
+  //document.getElementById().classList.add('dark')
   return (
     <div className="content">
       <Navbar />
