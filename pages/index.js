b87ed7c4eb461b0be3ea594d57344b59c424536b
oscar-halcon <oscar@secfi.com>
2022-12-08 12:59:38 +0100

    FinalizedApp

--- pages/index.js
+++ pages/index.js
@@ -11,16 +11,16 @@ export default function Home() {
         <title>Home</title>
       </Head>
       <div>
-        <h1>Homepage</h1>
+        <h1 className="flex mt-10">Homepage</h1>
         <p>
           Welcome to the comparison tool. If you want to know more about how it
           works you can click{" "}
           <Link href="/about">
-            <a>here</a>
+            <a><b>here</b></a>
           </Link>
           .{<br/>} And if you want to jump straight to the offers click{" "}
           <Link href="/offers/">
-            <a>here</a>
+            <a><b>here</b></a>
           </Link>
         </p>
       </div>
